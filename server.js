c46ea4372139e694c9b36b7cf27be2e84a3cb983
zechsoft <zechsoft.it@gmail.com>
2026-01-12 17:19:35 +0530

    ihih

--- server.js
+++ server.js
@@ -57,16 +57,24 @@ app.disable('x-powered-by');
 
 // Middleware
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'http://localhost:8080',
-        'http://127.0.0.1:5173',
-        'https://mansarafoods-o9z6.vercel.app',
-        process.env.FRONTEND_URL
-    ].filter(url => url && url !== 'undefined'),
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            'http://localhost:5173',
+            'http://localhost:8080',
+            'https://mansarafoods.com',
+            'https://www.mansarafoods.com'
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('CORS not allowed'));
+        }
+    },
     credentials: true,
     maxAge: 86400
 }));
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
