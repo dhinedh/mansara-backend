396d15d7459445a97c06bb7cd68961faeda71e9c
zechsoft <zechsoft.it@gmail.com>
2026-01-12 18:00:46 +0530

    K KJJ

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'X-Requested-With', 'Accept'],
 }));
 
 app.options(/(.*)/, cors());
